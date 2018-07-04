06847b20574891ef2adb70008716f997cdce1ee7
TeamOnGe <40074635+TeamOnGe@users.noreply.github.com>
2018-07-03 17:48:13 -0700

    Add files via upload

---
+++ CFW10/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CFW10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comPort = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.comPositions = new System.Windows.Forms.ComboBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtError = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtPos = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comPort
+            // 
+            this.comPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPort.FormattingEnabled = true;
+            this.comPort.Location = new System.Drawing.Point(11, 132);
+            this.comPort.Name = "comPort";
+            this.comPort.Size = new System.Drawing.Size(121, 24);
+            this.comPort.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(11, 184);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 32);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(175, 184);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(121, 32);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Disconnect";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(475, 127);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(121, 32);
+            this.btnMove.TabIndex = 7;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // comPositions
+            // 
+            this.comPositions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comPositions.FormattingEnabled = true;
+            this.comPositions.Items.AddRange(new object[] {
+            "Position 1",
+            "Position 2",
+            "Position 3",
+            "Position 4",
+            "Position 5",
+            "Position 6",
+            "Position 7",
+            "Position 8",
+            "Position 9",
+            "Position 10"});
+            this.comPositions.Location = new System.Drawing.Point(237, 132);
+            this.comPositions.Name = "comPositions";
+            this.comPositions.Size = new System.Drawing.Size(176, 24);
+            this.comPositions.TabIndex = 8;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(14, 49);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.ReadOnly = true;
+            this.txtPort.Size = new System.Drawing.Size(153, 22);
+            this.txtPort.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Connection:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(234, 112);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 17);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Position:";
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(237, 49);
+            this.txtError.Name = "txtError";
+            this.txtError.ReadOnly = true;
+            this.txtError.Size = new System.Drawing.Size(350, 32);
+            this.txtError.TabIndex = 20;
+            this.txtError.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtPos
+            // 
+            this.txtPos.Location = new System.Drawing.Point(593, 49);
+            this.txtPos.Name = "txtPos";
+            this.txtPos.ReadOnly = true;
+            this.txtPos.Size = new System.Drawing.Size(41, 32);
+            this.txtPos.TabIndex = 24;
+            this.txtPos.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::CFW10.Properties.Resources.cfw10;
+            this.pictureBox1.Location = new System.Drawing.Point(662, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 236);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtPos);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.comPositions);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.comPort);
+            this.Name = "Form1";
+            this.Text = "CFW10";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.ComboBox comPositions;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox txtError;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox txtPos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
